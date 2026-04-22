9d1778be10925ad21a15fb40c40b1f44efedc8fb
Chamanfaujdar1 <132120396+Chamanfaujdar1@users.noreply.github.com>
2026-04-22 11:23:30 +0530

    [Chaman Faujdar] Add.Added flowboard-web MVC layer with controllers and Thymeleaf views

---
+++ flowboard-web/src/main/java/com/flowboard/web/FlowboardWebApplication.java
@@ -0,0 +1,13 @@
+package com.flowboard.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowboardWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowboardWebApplication.class, args);
+	}
+
+}
---
+++ flowboard-web/src/test/java/com/flowboard/web/FlowboardWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowboard.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowboardWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
